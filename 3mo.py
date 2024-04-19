a6644be9bc8f0d6ae8fe578cd70753a9faebd739
szantomarcel <janoskopter9@gmail.com>
2024-04-19 11:48:16 +0200

    2. feladat

--- 3mo.py
+++ 3mo.py
@@ -3,3 +3,22 @@
 # b.	Hány meztelencsiga található az állományban? (Vegye figyelembe, hogy a félmeztelencsiga nem meztelencsiga!)
 # c.	Kérje be egy csiga latin nevét, majd ha megtalálható az adatok között, írja ki a hozzá tartozó magyar nevet! Oldja meg, hogy a keresés akkor is hozzon eredményt, ha a latin nevet nem nagy kezdőbetűvel kezdjük (a keresés legyen kis/nagybetűre érzéketlen)! Ha nem található ilyen latin név, azt is jelezzük a felhasználónak! A keresés során ügyeljen arra, hogy a keresést ne folytassa, ha a választ biztosan meg tudja adni!
 
+csiga={}
+csigak=[]
+
+with open("csigak.txt","r",encoding="utf-8")as bemenet:
+    for sor in bemenet:
+        adatok= sor.strip().split(";")
+        csiga["latin"]=adatok[0]
+        csiga["magyar"]=adatok[1]
+        csigak.append(csiga)
+        csiga={}
+                
+print(f"{len(csigak)} csiga adatai vannak a fajlban")
+
+
+db=0
+for i in range(len(csigak)):
+    if "meztelen" in csigak[i]["magyar"] and 'fél' not in csigak[i]["magyar"]:
+        db+=1
+print(f"{db} db meztelencsiga talalhato az adatok kozt")
